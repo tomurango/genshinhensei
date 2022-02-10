21de0f59cf63a0a1191d0202d0f5e29e91085fbc
Eiji TOMURA <s1f101803758@iniad.org>
2022-02-10 11:11:04 +0900

    20220210

---
+++ public/js/detailteam.js
@@ -0,0 +1,7 @@
+function detail_team(){
+    document.getElementById("detail_team_div").style.display = "block";
+}
+
+function detail_team_back(){
+    document.getElementById("detail_team_div").style.display = "none";
+}
