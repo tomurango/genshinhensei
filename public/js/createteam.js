1458ae8c0f24f94c7b621c6ee6a7ee148b2600f7
Eiji TOMURA <s1f101803758@iniad.org>
2022-02-14 15:33:30 +0900

    20220214

--- public/js/createteam.js
+++ public/js/createteam.js
@@ -5,3 +5,7 @@ function open_dialog_fab(){
 function open_dialog_fab_back(){
     document.getElementById("create_team_div").style.display = "none";
 }
+
+function choice_diakog(){
+    console.log("キャラ選択のダイアログを開く");
+}
