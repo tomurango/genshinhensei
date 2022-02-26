40002a44a65263bb5fe69d8c5243f0d0d4b7f9eb
Eiji TOMURA <s1f101803758@iniad.org>
2022-02-27 00:21:25 +0900

    20220227

--- public/js/createteam.js
+++ public/js/createteam.js
@@ -8,8 +8,15 @@ function open_dialog_fab_back(){
 
 function choice_diakog(){
     //console.log("choice_membar_dialog");
-    choice_membar_dialog.open();
+    //choice_membar_dialog.open();
+    document.getElementById("choice_membar_dialog").style.display = "block";
+}
+function choice_diakog_back(){
+    //console.log("choice_membar_dialog");
+    //choice_membar_dialog.open();
+    document.getElementById("choice_membar_dialog").style.display = "none";
 }
 
-
+/*
 var choice_membar_dialog = new mdc.dialog.MDCDialog(document.querySelector('#choice_membar_dialog'));
+*/
