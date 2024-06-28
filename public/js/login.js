7417ad3ec570c4abe287668a59bd21960378313c
tomurango <tomuraeishi@tomuraeishinoMacBook-Air.local>
2024-06-28 13:12:22 +0900

    PC版でログインできるようにした

--- public/js/login.js
+++ public/js/login.js
@@ -9,36 +9,34 @@ function login_card_display_back(){
     document.getElementById("login_card_div").style.display = "none";
 }
 
-
-//google
-function google_click(){
-    //ログインの動き
-    //console.log("google ログインします");
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function() {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        // Existing and future Auth states are now persisted in the current
-        // session only. Closing the window would clear any existing state even
-        // if a user forgets to sign out.
-        // ...
-        // New sign-in will be persisted with session persistence.
-        return firebase.auth().signInWithRedirect(provider).then(user =>{
-            // Get the user's ID token as it is needed to exchange for a session cookie.
-            return user.getIdToken();/*.then(idToken => {
-                // Session login endpoint is queried and the session cookie is set.
-                // CSRF protection should be taken into account.
-                // ...
-                const csrfToken = getCookie('csrfToken')
-                return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
-            });*/
-        });
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-    });
+function isMobile() {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /android|iPad|iPhone|iPod/.test(userAgent);
 }
+
+function google_click(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    //firebase.auth().signInWithRedirect(provider);
+    if (isMobile()) {
+        // モバイルデバイスの場合
+        firebase.auth().signInWithRedirect(provider);
+    } else {
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            var user = result.user;
+            console.log(user);
+            //ログインが成功したという処理
+            signedin_collect(user);
+            login_card_display_back();
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+    }
+}
+
 //ログアウト
 function log_out(){
     firebase.auth().signOut().then(()=>{
@@ -52,21 +50,13 @@ function log_out(){
 
 $(document).ready(function(){
     //chart js のグラフが表示されない問題の解決のための検証
-    firebase.auth().getRedirectResult().then(function(result) {
+    firebase.auth().getRedirectResult().then((result) => {
         //ログインの有無にかかわらずチームは取ってきて挿入するイメージ（後に分岐の可能性はある）
         get_teams();
-        //console.log(result);
-        if (result.credential) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
-        //console.log(token);
-        }
+        
         // The signed-in user info.
         global_user = result.user;
-        //console.log(global_user);
-        //console.log(result.user);
-        if(global_user != null){
+        if(result.user){
             // User is signed in.
             console.log("User is signed in");
             signedin_collect(result.user);
